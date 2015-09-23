b43895e00dd1bf2bdfc345eebdff580acefb44fe
vaibhav.gupta <vaibhav.gupta@hcentive.com>
2015-09-23 16:52:33 +0530

    Adding output file feature and whitelist support

--- textExtractor.py
+++ textExtractor.py
@@ -6,6 +6,8 @@ from HTMLParser import HTMLParser
 # constants
 ##################################################
 FILE_EXTENSIONS = '.jsp,'
+OMMIT_DIRECTORIES = ['depricated','target','exchange-admin-portal']
+WHITE_LISTED_CHARACTERS=['*','|','?','.',':','(',')','%','+','-','\'','$','[',']',',',':: ::','!','`']
 
 
 class MyHTMLParser(HTMLParser):
@@ -14,14 +16,12 @@ class MyHTMLParser(HTMLParser):
     current_tag = ''
 
     def handle_starttag(self, tag, attrs):
-        print "tag",tag
         self.current_tag = tag
         for attr in attrs:
             if (attr[0] == "title" or attr[0] == "alt"):self.text.append(attr[1])
     def handle_endtag(self, tag):
         self.current_tag = ''
     def handle_data(self, data):
-        print "data",data
         if self.current_tag != 'script' and self.current_tag != 'style':
             data_strip = data.strip()
             if data_strip: 
@@ -38,6 +38,7 @@ def sanitizeData(data):
     sanitizedData = removeBreakTag(sanitizedData)
     sanitizedData = removeTag("strong", sanitizedData)
     sanitizedData = removeMessageTagContent(sanitizedData)
+    sanitizedData = removeFormatTagContent(sanitizedData)
     sanitizedData = replaceAngularTagWithEl(sanitizedData)
     sanitizedData = removeCharacterEntityReference(sanitizedData)
     sanitizedData = removeTag("c:if", sanitizedData)
@@ -46,7 +47,6 @@ def sanitizeData(data):
 def reduce_data(data):
     output_str = ''
     for d in data:
-        #print d
         output_str = output_str + re.sub("\n|\t|\r","", d)
     return output_str
 
@@ -90,14 +90,21 @@ def removeMessageTagContent(data):
     regex = "(?:<s:message.*?/?>)|(?:</s:message>)"
     return re.sub(regex, '', data)
 
+def removeFormatTagContent(data):
+    regex = "(?:<fmt:formatDate.*?/?>)|(?:</fmt:formatDate>)"
+    return re.sub(regex, '', data)
+
 # process plain text
 def process_text(text):
     global current_file_path, file_path_static_text_map
     text_strip = text.strip()
-    if text_strip and (text_strip.find("<") == -1 and text_strip.find(">") == -1):
+    if text_strip and not white_listed(text_strip):
         file_path_static_text_map.setdefault(current_file_path, [])
         file_path_static_text_map[current_file_path].append(text)
     return text
+
+def white_listed(text):
+    return text.find("<") != -1 or text.find(">") != -1 or (text in WHITE_LISTED_CHARACTERS)
 
 # process a line
 def process_line(l):
@@ -165,11 +172,18 @@ def process_file(path):
 # process a directory
 def process_dir(path):
     for dirpath, dirnames, filenames in os.walk(path):
-        for fname in filenames:
-            fext = os.path.splitext(fname)[1]
-            if fext != '' and fext in FILE_EXTENSIONS.split(','):
-                fpath = os.path.join(dirpath, fname)
-                process_file(fpath)
+        if not ommitted_directory(dirpath):
+            for fname in filenames:
+                fext = os.path.splitext(fname)[1]
+                if fext != '' and fext in FILE_EXTENSIONS.split(','):
+                    fpath = os.path.join(dirpath, fname)
+                    process_file(fpath)
+
+def ommitted_directory(dirpath):
+    for ommitted_directory in OMMIT_DIRECTORIES:
+        if ommitted_directory in dirpath:
+            return True
+    return False
  
 def process_path(path):
     if os.path.exists(path):
@@ -178,16 +192,14 @@ def process_path(path):
         else:
             process_file(path)
 
-def print_file_path_static_text_map():
+def print_file_path_static_text_map(outputFile='output.html'):
     global file_path_static_text_map
     #print file_path_static_text_map
     table_str = ""
     for file_name, static_text_arr in file_path_static_text_map.iteritems():
         table_str += "<tr>"
-        print "filename" , file_name
         table_str += "<td rowspan='"+str(len(static_text_arr))+"'>" + file_name + "</td>"
         for i, static_text in enumerate(static_text_arr):
-            print "staticText", static_text
             static_text_col = "<td>"+static_text+"</td>"
             if i == 0:
                 table_str += static_text_col
@@ -195,7 +207,7 @@ def print_file_path_static_text_map():
                 table_str += "<tr>"+static_text_col+"</tr>"
         table_str += "</tr>"
     html_str = "<html><head><style>table, th, td {border: 1px solid black;}</style></head><body><table>"+table_str+"</table></body></html>"
-    f = open("output.html","w")
+    f = open(outputFile,"w")
     f.write(html_str)
     f.close();
  
@@ -207,7 +219,7 @@ import sys, os, re
 current_file_path = ''
 file_path_static_text_map = {}
  
-for path in sys.argv[1:]:
+for path in sys.argv[1:-1]:
     process_path(path)
 
-print_file_path_static_text_map()
+print_file_path_static_text_map(sys.argv[-1])
